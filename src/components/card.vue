7171b1967c13b44645f02d3cff22e74c6c2bd43a
Tomberryy <antoine.monnin@gmail.com>
2021-10-21 12:16:51 +0200

    Create card.vue
    Card créée!

---
+++ src/components/card.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <b-card no-body class="overflow-hidden" style="max-width: 540px">
+      <b-row no-gutters>
+        <b-col md="6">
+          <b-card-img
+            :src="thumbnail"
+            alt="Image"
+            class="rounded-0"
+          ></b-card-img>
+        </b-col>
+        <b-col md="6">
+          <b-card-body :title="title">
+            <b-card-text>
+              {{description}} <br/>
+              <b>Genre :</b> {{genre}}
+            </b-card-text>
+          </b-card-body>
+        </b-col>
+      </b-row>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "card",
+  props: {
+    title: {
+      type: String,
+      default: "",
+      required: true,
+    },
+    description: {
+      type: String,
+      default: "",
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      default: "",
+      required: true,
+    },
+    genre: {
+      type: String,
+      default: "",
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
