8bb06fdc6f0e713bada8021ba4f541103fee1183
Tomberryy <antoine.monnin@gmail.com>
2021-10-25 13:27:20 +0200

    uses api + remove json server

--- src/pages/result.vue
+++ src/pages/result.vue
@@ -1,38 +1,55 @@
 <template>
   <div>
-      <h1>Resultat</h1>
-      <div v-for="(jv, index) in jvs" :key="index">
-        <card :description="jv.short_description" :genre="jv.genre" :thumbnail="jv.thumbnail" :title="jv.title"/>
-      </div>
+    <h1>Resultat</h1>
+    <div v-for="(jv, index) in jvs" :key="index">
+      <card
+        :description="jv.short_description"
+        :genre="jv.genre"
+        :thumbnail="jv.thumbnail"
+        :title="jv.title"
+        :id="jv.id"
+        @clicked="childClicked"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import card from "../components/card.vue"
+import card from "../components/card.vue";
 const axios = require("axios").default;
 export default {
-    name:"result",
-    components : {
-      card
+  name: "result",
+  components: {
+    card,
+  },
+  data() {
+    return {
+      jvs: [],
+    };
+  },
+  mounted() {
+    this.getVg();
+  },
+  methods: {
+    async getVg() {
+      const options = {
+        method: "GET",
+        url: "https://free-to-play-games-database.p.rapidapi.com/api/games",
+        headers: {
+          "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+          "x-rapidapi-key":
+            "7d72930235msh2d96d13e2a5ba40p1d6df0jsnd7cfe694c4a2",
+        },
+      };
+      const temp = await axios.request(options);
+      this.jvs = temp.data;
     },
-    data() {
-      return {
-        jvs : []
-      }
+    childClicked(val) {
+      console.log(val);
     },
-    mounted(){
-      this.getVg()
-    },
-    methods : {
-      async getVg() {
-        const temp = await axios.get("http://localhost:3000/jv");
-        this.jvs = temp.data;
-        console.log(this.jvs[1]);
-      }
-    }
-}
+  },
+};
 </script>
 
 <style>
-
 </style>
