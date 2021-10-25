1fdd4faa7d50fb362023ebd8024ee7ce50a1137f
Martinez David <79132377+FragThor@users.noreply.github.com>
2021-10-25 13:30:34 +0200

    about us

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,14 +1,32 @@
 <template>
-  <h1>A propos</h1>
+<v-container>
+    <div :class="[`text-h2`]" class="transition-swing">About Us</div>
+
+        Face à tout le mal que l'enfer peut enfanter, toute la vilenie que l'humanité peut produire, nous n'enverrons contre eux, qu'ADL Gaming.
+
+
+        ADL Gaming s'est construite sur des valeurs fortes : passion, satisfaction client, service, partage, innovation et conseil. 
+        Il ne s'agit pas de vendre pour vendre, mais de proposer le bon produit à la bonne personne, dans une relation de confiance.
+
+ <div class="bg-image">
+    <img
+      src="../assets/doom.jpg"
+      class="img-fluid"
+      alt="Sample"
+    />
+    <div class="mask" style="background-color: rgba(0, 0, 0, 0.6)">
+      <div class="d-flex justify-content-center align-items-center h+50">
+        <p class="text-white mb-0">ADL Gaming</p>
+      </div>
+    </div>
+  </div>
+
+  </v-container>
+  
 </template>
 
 <script>
 export default {
-  name:"about"
-
-}
+  name: "AboutUs",
+};
 </script>
-
-<style>
-
-</style>
