9250e24e3f7611dc671d2cba3ebb776fd27282e6
Tomberryy <antoine.monnin@gmail.com>
2021-10-25 09:46:44 +0200

    Début de get des données api

--- src/pages/result.vue
+++ src/pages/result.vue
@@ -1,13 +1,33 @@
 <template>
   <div>
       <h1>Resultat</h1>
+      <card description="test" genre="test" thumbnail="https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png" title="test"/>
   </div>
 </template>
 
 <script>
+import card from "../components/card.vue"
+const axios = require("axios").default;
 export default {
-    name:"result"
-
+    name:"result",
+    components : {
+      card
+    },
+    data() {
+      return {
+        jv : []
+      }
+    },
+    mounted(){
+      this.getVg()
+    },
+    methods : {
+      async getVg() {
+        const temp = await axios.get("https://www.freetogame.com/api/games");
+        this.jv = temp;
+        console.log(this.jv);
+      }
+    }
 }
 </script>
 
