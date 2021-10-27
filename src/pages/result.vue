3f6925ccfe2ac73400fcaa4bc86a4bc37e65ea53
Tomberryy <antoine.monnin@gmail.com>
2021-10-27 15:19:03 +0200

    Merge search into result page

--- src/pages/result.vue
+++ src/pages/result.vue
@@ -1,20 +1,42 @@
-<template>
+ <template>
   <div>
-    <h1>Resultat</h1>
+    <br />
+    <h1>Recherche</h1>
 
-    <!-- Display all games loop -->
-    <div v-for="(jv, index) in jvs" :key="index">
-      <card
-        :description="jv.short_description"
-        :genre="jv.genre"
-        :thumbnail="jv.thumbnail"
-        :title="jv.title"
-        :id="jv.id"
-        @clicked="childClicked"
+    <!-- Seach input div -->
+    <div id="recherche">
+      <input
+        type="text"
+        v-model="search"
+        @keyup="filterJv"
+        v-on:keyup.delete="filterJv"
       />
     </div>
+
+    <h1>Resultat</h1>
+
+    <!-- If just loaded and waiting for API -->
+    <div v-if="jvs.length === 0">loading...</div>
+    
+    <!-- Display search results (Only if search array is filled) -->
+    <div v-else-if="jvsSearch.length !== 0">
+      <div v-for="(jv, index) in jvsSearch" :key="index">
+        <card
+          :description="jv.short_description"
+          :genre="jv.genre"
+          :thumbnail="jv.thumbnail"
+          :title="jv.title"
+          :id="jv.id"
+          @clicked="childClicked"
+        />
+      </div>
+    </div>
+
+    <!-- Else no result message -->
+    <div v-else>Aucun resultat!</div>
+    
+    <!-- The modal -->
     <div>
-      <!-- The modal -->
       <b-modal id="my-modal"
         ><card-detail
           :title="singleJv && singleJv.title"
@@ -37,7 +59,9 @@ import card from "../components/card.vue";
 //More detailed card for single game modal
 import cardDetail from "../components/cardDetail.vue";
 
+//Axios
 const axios = require("axios").default;
+
 export default {
   name: "result",
   components: {
@@ -46,21 +70,21 @@ export default {
   },
   data() {
     return {
-      jvs: [],         //Json array with all the games' info
-      id: 1,           //Game index for single game display
-      singleJv : null  //Single videogame display content
+      jvs: [],        //All games array
+      search: "",     //Seach box content
+      id: 1,          //Index for single videogame display
+      jvsSearch: [],  //Search result array
+      singleJv: null, //Single videogame content
     };
   },
-
-  //gets all videogames at init
   mounted() {
-    this.getVg();
+    this.getVg();  //At mount, get all videogames
   },
   methods: {
-    //Gets all the videogames from api
+    //Get all videogames and store it in jvs array
     async getVg() {
-      let options = {
-        //API settings, keys, method
+      //API settings, method, URL, keys
+      const options = {
         method: "GET",
         url: "https://free-to-play-games-database.p.rapidapi.com/api/games",
         headers: {
@@ -69,18 +93,29 @@ export default {
             "7d72930235msh2d96d13e2a5ba40p1d6df0jsnd7cfe694c4a2",
         },
       };
-
       //Do request
-      let temp = await axios.request(options);
-
-      //Get data into jvs array
+      const temp = await axios.request(options);
+      //Store result in jvs array
       this.jvs = temp.data;
+      this.jvsSearch = this.jvs;
+    },
+    filterJv() {
+      //Get search value from text box
+      const s = this.search;
+      this.jvsSearch = [];
+
+      //Look for results in array with filter parameter
+      const filter = this.jvs.filter(function (u) {
+        return u.title.includes(s);
+      });
+      //Put search result in searched videogames array
+      this.jvsSearch = filter;
     },
 
     //When child is clicked, get id from child back, and use said ID to display single game
     async childClicked(val) {
       let options = {
-        //API settings, keys, method, request params (ID)
+        //API settings, keys, method, request params
         method: "GET",
         url: "https://free-to-play-games-database.p.rapidapi.com/api/game",
         params: { id: val },
@@ -90,12 +125,11 @@ export default {
             "3b6fde0462msh8f59fe31774848ap1a098cjsn4821d8bafdfb",
         },
       };
-
       //Do request
       let temp = await axios.request(options);
       //Show modal
-      this.$bvModal.show('my-modal')
-      //Get request result into singleJv variable
+      this.$bvModal.show("my-modal");
+      //Assign data from single videogame to singeJv
       this.singleJv = temp.data;
     },
   },
@@ -104,3 +138,4 @@ export default {
 
 <style>
 </style>
+
--- src/plugin/router.js
+++ src/plugin/router.js
@@ -7,15 +7,12 @@ import home from "../pages/home.vue";
 import contact from "../pages/contact.vue";
 import result from "../pages/result.vue";
 import about from "../pages/about.vue";
-import recherche from "../pages/recherche.vue";
 
 const routes = [
     { path: '/', component: home },
     { path: '/contact', component: contact },
     { path: '/about', component: about },
     { path: '/result', component: result },
-    { path: '/recherche', component : recherche},
-    
 ]
 
 const router = new VueRouter({
